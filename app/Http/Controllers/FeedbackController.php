dd9f6bbc9d0aff99389fe313da6da247b76a21fb
bubugihu <59214991+bubugihu@users.noreply.github.com>
2020-07-10 11:03:29 +0700

    add records banner, feedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -15,7 +15,7 @@ class FeedbackController extends Controller
             'feedbackName'          =>      'bail|required|regex:/^[a-zA-Z ]{2,}/i|max:255',
             'feedbackPhone'         =>      'bail|nullable|regex:/^0[1-9][0-9]{8,9}$/i',
             'feedbackEmail'         =>      'bail|required|regex:/^[a-zA-Z0-9\._]+@[a-zA-Z0-9_]+\.[a-zA-Z0-9]+[\.a-zA-Z0-9]*$/i',
-            'feedbackContent'       =>      'bail|required|min:2|max:150',
+            'feedbackContent'       =>      'bail|required|min:2|max:250',
         ]);
 
         $feedback = new Feedback();
